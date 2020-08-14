9e3ab0844afe320ef7440363eb87b8a778775233
t0masz <tomas.zikmund@email.cz>
2020-08-14 08:41:32 +0200

    nastavení použití gmaps v cookies

--- ezer_main.php
+++ ezer_main.php
@@ -44,7 +44,7 @@
   $jQuery=   isset($_GET['jquery'])  ? $_GET['jquery']  : (isset($_COOKIE['jquery']) ? $_COOKIE['jquery']  : '3.3.1');
   $dbg=      isset($_GET['dbg'])     ? $_GET['dbg']     : 0;
   $gapi=     isset($_GET['gapi'])    ? $_GET['gapi']    : 0; //!($ezer_local || $ezer_ksweb);
-  $gmap=     isset($_GET['gmap'])    ? $_GET['gmap']    : ($is_local?0:1);
+  $gmap=     isset($_GET['gmap'])    ? $_GET['gmap']    : (isset($_COOKIE['gmap']) ? $_COOKIE['gmap']  : ($is_local?0:1));
   $touch=    isset($_GET['touch'])   ? $_GET['touch']   : 1;
 
   // inicializace SESSION
