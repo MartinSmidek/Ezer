f8357a65a962b6ef3d96090b94aa6d607a603043
MartinSmidek <martin@smidek.eu>
2020-08-31 12:28:05 +0200

    * vrácení úpravy set_cookie - s parametrem 'sameSite=Strict;secure' se cookie nezapíše

--- client/ezer_app3.js
+++ client/ezer_app3.js
@@ -3467,7 +3467,7 @@ Ezer.fce.set_cookie= function (id,val='',form=null,refs=null) {
   let duration= 100, // days
       date= new Date();
   date.setTime(date.getTime() + duration * 24 * 60 * 60 * 1000);
-  document.cookie= id + '=' + encodeURIComponent(v) + ';Expires=' + date.toGMTString()+';sameSite=Strict;secure';
+  document.cookie= id + '=' + encodeURIComponent(v) + ';Expires=' + date.toGMTString();
   return 1;
 };
 //--------------------------------------------------------------------------------------- get_cookie
