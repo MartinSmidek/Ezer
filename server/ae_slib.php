107dc89dce3836b4432040c609e8d0f1308a7b78
MartinSmidek <martin@smidek.eu>
2025-10-18 12:35:51 +0200

    * oprava cesty phpexcel

--- server/ae_slib.php
+++ server/ae_slib.php
@@ -1886,7 +1886,7 @@ function Excel2007($desc) {
 function Excel5($desc,$gen=1,&$wb=null,$dir='',$excel='xls') {  #trace();
   global $ezer_path_root, $ezer_version;
   // natáhneme knihovny
-//  require_once "ezer$ezer_version/server/vendor/autoload.php";
+  require_once "ezer$ezer_version/server/vendor/autoload.php";
   // pro testování a vývoj
   $list= false;
   if (!$desc || $desc=='0') {
