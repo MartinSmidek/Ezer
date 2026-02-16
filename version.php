824bc45285b239521cfe5b88faf4490b33bb5983
MartinSmidek <martin@smidek.eu>
2026-02-16 17:29:08 +0100

    * oprava chování při vypršení session

--- version.php
+++ version.php
@@ -1,4 +1,5 @@
 <?php
-  $version=40; // nová událost onempty se aktivuje pokud browse_load vrátí 0 řádků
+  $version=41; // lepší chování při odhlášení z důvodu nečinnosti - expirace SESSION
+//$version=40; // nová událost onempty se aktivuje pokud browse_load vrátí 0 řádků
 //$version=39; // vráceno form.init z verze 3.1 ... znemožnilo práci s format:'x' tj. s fixovanými položkami ... označeno jako //20251203  
 ?>
