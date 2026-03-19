0a4da959043ef5acc8e37a0faa2853fc895d85a7
MartinSmidek <martin@smidek.eu>
2026-03-19 16:34:07 +0100

    * dialog prompt je interpretován jako prompt2
    * oprava debugeru
    * oprava kompilátoru

--- version.php
+++ version.php
@@ -1,5 +1,6 @@
 <?php
-  $version=43; // úprava odhlášení uživatele
+  $version=44; // oprava hlášen chyby kompilace this=, oprava debugeru 
+//$version=43; // úprava odhlášení uživatele
 //$version=42; // přidány mapy OpenStreet - v url musí být &omap=1
 //$version=41; // lepší chování při odhlášení z důvodu nečinnosti - expirace SESSION
 //$version=40; // nová událost onempty se aktivuje pokud browse_load vrátí 0 řádků
