3b99fc888bf94d4c019dce099bef948184eb6691
MartinSmidek <martin@smidek.eu>
2026-04-01 16:34:27 +0200

    * opravy LabelMap esp. fit_bounds a set mark CIRCLE

--- version.php
+++ version.php
@@ -1,5 +1,6 @@
 <?php
-  $version=46; // na mapě OpenStreet lze zobrazit více polygonů
+  $version=47; // opravy LabelMap esp. fit_bounds a set mark CIRCLE
+//$version=46; // na mapě OpenStreet lze zobrazit více polygonů
 //$version=45; // oprava funkce prompt tj. prompt2 ... tlačítko Zpět/ESC vrací default
 //$version=44; // oprava hlášen chyby kompilace this=, oprava debugeru 
 //$version=43; // úprava odhlášení uživatele
